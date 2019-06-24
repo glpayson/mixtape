4c3909110a0688ad861bdf458d32087ce41dbe50
Lamar Payson <glpayson@gmail.com>
2019-06-24 07:44:47 -0400

    Add a few more docstrings. Fix usage string.

--- src/mixtape/core.clj
+++ src/mixtape/core.clj
@@ -3,9 +3,12 @@
             [mixtape.editor :as editor]
             [mixtape.io.writer :as writer]))
 
-(def ^:private usage "USAGE: lein run -m mixtape.core mixtape.json changes.json output-file.json")
+(def ^:private usage "USAGE: lein run -m mixtape.core mixtape.json changes.edn output-file.json")
 
 (defn -main
+  "Takes a mixtape json file and an edn file containing lists of changes to apply to the mixtape.
+  The edited mixtape is written, in json format, to an output file.
+  USAGE: lein run -m mixtape.core mixtape.json changes.edn output-file.json"
   [& args]
   (if-not (= 3 (count args))
     (println usage)
--- src/mixtape/io/reader.clj
+++ src/mixtape/io/reader.clj
@@ -4,12 +4,16 @@
             [camel-snake-kebab.core :as csk]
             [camel-snake-kebab.extras :as cske]))
 
-(defn read-mixtape [mixtape-file]
+(defn read-mixtape
+  "Reads (json) mixtape file and coerces its keys to kebab-cased keywords."
+  [mixtape-file]
   (as-> (slurp mixtape-file) %
         (ch/parse-string % true)
         (cske/transform-keys csk/->kebab-case-keyword %)))
 
-(defn read-edits [changes-file]
+(defn read-edits
+  "Reads edn changes file."
+  [changes-file]
   (-> (slurp changes-file)
       (edn/read-string)))
 
